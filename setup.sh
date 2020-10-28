bfc373311fabae9f65464ccc2635c33725038cf9
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2020-10-28 10:35:29 -0300

    Setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-source scripts/main.sh
+set -e
+
+::source () {
+  source "$(dirname -- "$(readlink -f -- $BASH_SOURCE)")/$1"
+}
+
+::source scripts/log.sh
+::source scripts/util.sh
+::source scripts/packages.sh
+::source scripts/config.sh
+
+packages::install_packages
+
+config::setup_git
+config::setup_vim
+config::setup_tmux
+config::setup_bash
+
+set +e
