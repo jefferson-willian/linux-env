7089ff17c18b472111c377958d5b20db3a393709
Jefferson Monteiro <jefferson.willian.monteiro@gmail.com>
2022-10-30 09:47:14 -0400

    Improve logging for installing packages.

--- lib/packages.sh
+++ lib/packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 function packages::install_package () {
+  readonly has_sudo=$(sudo -n true 2> /dev/null && echo 0 || echo 1)
+  if [[ $has_sudo -ne 0 ]]; then
+    log::info "Please run as root to install packages"
+  fi
   sudo apt-get -y install $1 > /dev/null
 }
 
@@ -9,17 +13,25 @@ function packages::is_package_installed () {
 }
 
 function packages::install_packages() {
+  total=0
+  installed=0
+  skipped=0
+
+  log::info "Installing packages..."
   while read -r line
   do
     package=$(echo "$line" | awk '{print $1}')
     if [[ ! -z $package ]] && [[ ! "#" = $package ]] ; then
+      total=$((total+1))
       if ! $(packages::is_package_installed $package); then
-        log::progress "Installing $package"
+        installed=$((installed+1))
         packages::install_package $package
-        log::progress_done
       else
-        log::info "$package already installed!"
+        skipped=$((skipped+1))
       fi
     fi
   done < "$1"
+
+  log::info "(${installed}/${total}) packages installed."
+  log::info "(${skipped}/${total}) packages already installed."
 }
