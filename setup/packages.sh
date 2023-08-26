2329335558eecce3d0844141175453d16adff4cb
Jefferson Willian Monteiro <jefferson.willian.monteiro@gmail.com>
2023-08-26 22:05:58 +0000

    Can only install packages if apt-get in available

--- setup/packages.sh
+++ setup/packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 setup::packages () {
-  packages::install_packages "$(dirname -- "$(readlink -f -- $0)")/config/packages"
+  # Can only install packages if apt-get in available
+  if [[ ! -z $(command -v apt-get) ]]; then
+    packages::install_packages "$(dirname -- "$(readlink -f -- $0)")/config/packages"
+  fi
 }
